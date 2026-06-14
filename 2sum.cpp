7d97fc7771c1fbde6a7b50c58fba586c26f64703
ATHIPATLA-JASWITHA <jaswitha.23BCB7267@vitapstudent.ac.in>
2026-06-14 14:56:34 +0530

    problem -2

--- 2sum.cpp
+++ 2sum.cpp
@@ -19,3 +19,48 @@ public:
         return ans;
     }
 };
+
+/**
+ * 2. Add Two Numbers
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+                 int carry=0;
+         ListNode* LL1=l1;
+         ListNode* LL2=l2;
+         ListNode* dmynode=new ListNode(0);
+         ListNode*temp=dmynode;
+         while(LL1!=NULL || LL2!=NULL ||carry!=0){
+            int sum=0;
+            if(LL1!=NULL){
+                  sum+=LL1->val;
+                  LL1=LL1->next;
+            }
+            if(LL2!=NULL){
+                sum+=LL2->val;
+                LL2=LL2->next;
+            }
+           sum+=carry;
+           carry=sum/10;
+           ListNode*newNode=new ListNode(sum%10);
+           temp->next=newNode;
+           temp=temp->next;
+
+         }
+         if(carry){
+            ListNode*newNode=new ListNode(carry);
+            newNode->next=dmynode;
+            return newNode;
+         }                    
+         return dmynode->next;
+    }
+};
