5b0994665c72c6c7294ced424617a57a8e061b21
ATHIPATLA-JASWITHA <jaswitha.23BCB7267@vitapstudent.ac.in>
2026-06-27 23:13:13 +0530

    palindrome easy question weekend

---
+++ Palindrome.cpp
@@ -0,0 +1,16 @@
+//9. Palindrome Number
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+                long long int ans=0;
+        int originalans=x;
+        while(x>0){
+            int rem=x%10;
+            ans=(ans*10)+rem;
+            x=x/10;
+        }
+        if(originalans!=ans)return false ;
+        return true;
+    }
+};
